ccde079ff6485f8afb82a9be8812f03fd3ae34aa
chulwoopack <chulwoo.pack@gmail.com>
2018-03-01 16:30:44 -0600

    Updated bash to skip non-folder file under test1 and test2 folder

--- RunPageSegmentation.sh
+++ RunPageSegmentation.sh
@@ -9,15 +9,30 @@ src=$(pwd)/src
 cd data/FullPages/
 totalCount=0
 for i in $( ls ); do
-    cd $i
-    echo $i
+    if [[ -d $i ]]; then
+        cd $i
+        echo $i
+    else
+        echo "$i is not a folder"
+        continue
+    fi
     for j in $( ls ); do
-    year=`echo $j | awk -F "_|-" {'print $2'}`
+        if [[ -d $j ]]; then
+            year=`echo $j | awk -F "_|-" {'print $2'}`
+        else
+            echo "$j is not folder"
+            continue
+        fi
     #if [ "$year" -ge "$beginYear" ] && [ "$year" -le "$endYear" ]
     if [ 1 ]
     then
-        cd $j
-        echo $j
+        if [[ -d $j ]]; then
+            cd $j
+            echo $j
+        else
+            echo "$j is not a folder"
+            continue
+        fi
         for k in $( ls ); do
             totalCount=$((totalCount + 1))
             current=$(pwd)
